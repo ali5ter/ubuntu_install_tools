74e0d0b872d7e5a92fdc554a1227de0fc0a44d0e
Alister Lewis-Bowen <alister@different.com>
2015-11-30 15:05:54 -0500

    more OS patching

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Ubuntu Install Tools
-
 Scripts to help speed up the installation and configuration of an ubuntu server.
 
 Tested for installations of:
@@ -12,18 +11,17 @@ Tested for installations of:
 ## Configuration
 Run the `post_install.sh` script. You could run it without cloning this repository using
 
-    $(wget -O - http://gitlab.different.com/alister/ubuntu_install_tools/raw/master/post_install.sh | bash)
-    
+    $(wget -qO - http://gitlab.different.com/alister/ubuntu_install_tools/raw/master/post_install.sh | bash)
+
 You can change the defaults located at the top of this shell script or change them from teh command line. To do this with the command above use
 
-    $(wget -O - http://gitlab.different.com/alister/ubuntu_install_tools/raw/master/post_install.sh | bash -s <hostname> <domain> <username> <password>)
-    
+    $(wget -qO - http://gitlab.different.com/alister/ubuntu_install_tools/raw/master/post_install.sh | bash -s <hostname> <domain> <username> <password>)
+
 ## Contribution
-If you've stumbled upon this project and wish to contribute, please 
-[let me know](mailto:alister@different.com).
+If you've stumbled upon this project and wish to contribute, please [let me know](mailto:alister@different.com).
 
 ## Reference
-* [Unattended insallation](http://askubuntu.com/questions/122505/how-do-i-create-a-completely-unattended-install-of-ubuntu)
+* [Unattended installation](http://askubuntu.com/questions/122505/how-do-i-create-a-completely-unattended-install-of-ubuntu)
 * [Additional recommended steps](https://www.digitalocean.com/community/tutorials/additional-recommended-steps-for-new-ubuntu-14-04-servers)
 * [Post install using puppet](https://github.com/netson/ubuntu-unattended/blob/master/start.sh)
 * [Carrybag](http://gitlab.different.com/alister/carrybag/)
--- post_install.sh
+++ post_install.sh
@@ -3,10 +3,14 @@
 
 set -e
 
+# ============================================================================
+# Environment
+
 VERSION="$(lsb_release -cs)"
 HOSTNAME="$(hostname)"
 DOMAIN=".local"
 USER="admin"
+PASSWD="\\!QAZ2wsx"
 tmp="/tmp"
 
 [ "$(id -u)" != "0" ] && {
@@ -18,12 +22,16 @@ tmp="/tmp"
     stty sane
     read -ep "Preferred hostname: " -i "$HOSTNAME" HOSTNAME
     read -ep "Preferred domain: " -i "$DOMAIN" DOMAIN
-    read -ep "Preferred username: " -i "$USER" USER
+    read -ep "Preferred admin username: " -i "$USER" USER
+    read -ep "Preferred admin password: " -i "$PASSWD" PASSWD
 }
 
 FQDN="$HOSTNAME.$DOMAIN"
 
 echo "Configuring this server..."
+
+# ============================================================================
+# Patch the OS
 
 echo "$HOSTNAME" > /etc/hostname
 sed -i "s@ubuntu.ubuntu@$FQDN@g" /etc/hosts
@@ -36,14 +44,32 @@ apt-get dist-upgrade -y > /dev/null 2>&1
 apt-get autoremove -y > /dev/null 2>&1
 apt-get purge -y > /dev/null 2>&1
 
-UDIR="~$USER"
-## TODO: Set up alternate account
-## TODO: Configure sshd
-## TODO: Lock down
-
+apt-get install -y openssh-server > /dev/null 2>&1
 apt-get install -y open-vmware-tools > /dev/null 2>&1
 apt-get install -y git > /dev/null 2>&1
-apt-get install -y htop > /dev/null 2>&1
+
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+service ssh restart
+
+ufw allow ssh
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 443/tcp
+sudo ufw enable
+
+fallocate -l 4G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+
+# ============================================================================
+# Alternate root account
+
+UDIR="~$USER"
+EPASSWD="$(openssl passwd -crypt $PASSWD)"
+adduser --defaults --password "$EPASSWD" "$USER" > /dev/null 2>&1
+gpasswd -a "$USER" sudo > /dev/null 2>&1
 
 SRC="$UDIR/src"
 su "$USER" <<"END_OF_ADMIN_COMMANDS"
@@ -54,6 +80,9 @@ git clone http://gitlab.different.com/alister/carrybag.git > /dev/null 2>&1
 END_OF_ADMIN_COMMANDS
 
 exit 0
+
+# ============================================================================
+# Clean up
 
 rm $0
 echo "Configuration complete. Going to reboot..."

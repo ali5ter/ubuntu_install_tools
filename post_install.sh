81a46d88e6c2d0ec02dc2ae03e524a26c690abc3
Alister Lewis-Bowen <alister@different.com>
2015-12-01 08:25:42 -0500

    add motd config and parse params from the cmdl

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # @file post_install.sh
 # Post installation script to configure Ubuntu
-# @see https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-14-04
 # @see https://www.digitalocean.com/community/tutorials/additional-recommended-steps-for-new-ubuntu-14-04-servers
 # @see https://github.com/netson/ubuntu-unattended
 # @see http://gitlab.different.com/alister/carrybag/
@@ -11,8 +10,8 @@ set -e
 # ============================================================================
 # Environment
 
-HOSTNAME="$(hostname)"
-DOMAIN=".local"
+HOSTNAME="${1:-$(hostname)}"
+DOMAIN="${2:-'.local'}"
 
 [ "$(id -u)" != "0" ] && {
     echo "This script should be run as root" 1>&2
@@ -40,24 +39,34 @@ apt-get dist-upgrade -y > /dev/null 2>&1
 apt-get autoremove -y > /dev/null 2>&1
 apt-get purge -y > /dev/null 2>&1
 
-#apt-get install -y openssh-server > /dev/null 2>&1
+apt-get install -y openssh-server > /dev/null 2>&1
 apt-get install -y open-vmware-tools > /dev/null 2>&1
 apt-get install -y git > /dev/null 2>&1
 
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 service ssh restart
 
-ufw allow ssh
-ufw allow 80/tcp
-ufw allow 443/tcp
-ufw allow 443/tcp
-ufw enable
+#ufw allow ssh
+#ufw allow 80/tcp
+#ufw allow 443/tcp
+#ufw allow 25/tcp
+#ufw enable
 
 fallocate -l 4G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+
+apt-get install -y figlet > /dev/null 2>&1
+echo > /etc/motd
+{ \
+    figlet "$HOSTNAME"; \
+    echo -n "\nWelcome to $HOSTNAME\n"; \
+    echo "Any malicious and/or unauthorized activity is strictly forbidden."; \
+    echo "All activity may be logged."; \
+} >> /etc/motd
+apt-get purge figlet > /dev/null 2>&1
 
 # ============================================================================
 # Alternate root account
@@ -68,6 +77,7 @@ su "$USER" <<"END_OF_ADMIN_COMMANDS"
 mkdir "$SRC" > /dev/null 2>&1
 cd "$SRC" > /dev/null 2>&1
 git clone http://gitlab.different.com/alister/carrybag.git > /dev/null 2>&1
+cd carrybag > /dev/null 2>&1
 ./tools/install.sh -q > /dev/null 2>&1
 END_OF_ADMIN_COMMANDS
 

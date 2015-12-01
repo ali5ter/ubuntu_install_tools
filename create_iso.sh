c3dd74a803ef978053a416337537d2bb07a78cc0
Alister Lewis-Bowen <alister@different.com>
2015-12-01 16:03:55 -0500

    clean up spinner animation

--- create_iso.sh
+++ create_iso.sh
@@ -9,16 +9,14 @@ set -e
 spinner() {
     # @see http://fitnr.com/showing-a-bash-spinner.html
     local pid=$1
-    local delay=0.75
     local spinstr='|/-\'
     while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
         local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
+        printf "%c" "$spinstr"
         local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
+        printf "\b\b"
     done
-    printf "    \b\b\b\b"
+    printf "\b\b"
 }
 
 download() {
